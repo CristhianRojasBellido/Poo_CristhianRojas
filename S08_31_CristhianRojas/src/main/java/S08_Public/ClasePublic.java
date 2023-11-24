a25b259038fcb1d1a1d48fe397361ba63a02764e
CristhianRojas <131499371+CristhianRojasBellido@users.noreply.github.com>
2023-11-23 20:02:54 -0500

    S08 resuelto

--- S08_31_CristhianRojas/src/main/java/S08_Public/ClasePublic.java
+++ S08_31_CristhianRojas/src/main/java/S08_Public/ClasePublic.java
@@ -1,4 +1,4 @@
-/*
+    /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
@@ -7,7 +7,7 @@ package S08_Public;
 public class ClasePublic {
     //Atributo
     String nombre = "Cristhian";
-    String edad = "19";
+    int edad = 19;
     //Metodo
     public void saludar(){
         System.out.println("Hola, Mi nombre es " + nombre);
